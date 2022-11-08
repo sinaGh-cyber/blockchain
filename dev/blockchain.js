a5636d7ecede92e44f439d6ea9dc2c4dc0c58707
Sina gholami <mohammad.sina.gholami.cyber@gmail.com>
2022-11-08 22:52:25 +0330

    blockchain explorer end points added

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -66,7 +66,7 @@ class Blockchain {
     return nonce;
   }
 
-  chainIsValid = (blockchain) => {
+  chainIsValid(blockchain) {
     let validChain = true;
 
     for (let i = 1; i < blockchain.length; i++) {
@@ -89,11 +89,62 @@ class Blockchain {
     const correctHash = genesisBlock.hash === '0';
     const correctTransactions = genesisBlock.transaction.length === 0;
 
-    if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) {
+    if (
+      !correctNonce ||
+      !correctPreviousBlockHash ||
+      !correctHash ||
+      !correctTransactions
+    ) {
       validChain = false;
     }
     return validChain;
-  };
+  }
+
+  getBlock(blockHash) {
+    let correctBlock = null;
+    this.chain.forEach((block) => {
+      if (block.hash === blockHash) correctBlock = block;
+    });
+    return correctBlock;
+  }
+
+  getTransaction(transactionId) {
+    let correctTransaction = null;
+    let correctBlock = null;
+    this.chain.forEach((block) => {
+      block.transactions.forEach((transaction) => {
+        if (transaction.transactionId === transactionId) {
+          correctTransaction = transaction;
+          correctBlock = block;
+        }
+      });
+    });
+    return { transaction: correctTransaction, block: correctBlock };
+  }
+
+  getAddressData(address) {
+    const addressTransactions = [];
+    this.chain.forEach((block) => {
+      block.forEach((transaction) => {
+        if (
+          transaction.sender === address ||
+          transaction.recipient === address
+        ) {
+          addressTransactions.push(transaction);
+        }
+      });
+    });
+
+    let balance = 0;
+    addressTransactions.forEach((transaction) => {
+      if (transaction.recipient === address) balance += transaction.amount;
+      else if (transaction.sender === address) balance -= transaction.amount;
+    });
+    return {
+      addressTransactions,
+      addressBalance: balance,
+    };
+  }
 }
 
 module.exports = Blockchain;
